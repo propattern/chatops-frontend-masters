7cfd853f1dd962539e204641699fae5951ed6a7c
Iqbal Ahmed <admin@propattern.com>
2023-09-26 01:19:13 +0100

    chore: [no ticket]: a point where dms can e sent

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,146 @@
+import fetch from "node-fetch";
 import type { Handler } from '@netlify/functions';
 
-export const handler: Handler = async () => {
-	// TODO validate the Slack request
+import {parse} from 'querystring'
+import { blocks, getUsernames, modal, slackApi, verifySlackRequest } from './util/slack';
 
-	// TODO handle slash commands
+const  handleSlashCommand= async (payload: SlackSlashCommandPayload) => {
+	switch(payload.command) {
+		case '/foodfight':
+			const response = await slackApi('views.open', 
+			modal({
+					id: 'foodfight-modal',
+					title: 'Start a food fight',
+					trigger_id: payload.trigger_id,
+					blocks: [
+						blocks.section({
+							text: 'Experimentation Feedback"'
+						}),
+						blocks.radios({
+							id: 'spice_level',
+							label: 'How satisfied are you with the support you receive from the team?',
+							placeholder: 'Select a spice level',
+							options: [
+								{label: '1  Not at all satisfied', value: '1'},
+								{label: '2', value: '2'},
+								{label: '3', value: '3'},
+								{label: '4', value: '4'},
+								{label: '5 = Very satisfied', value: '5'},
+							]
+						}),
+						blocks.input({
+							id: 'opinion',
+							label: 'Please tell us why you answered the way you did',
+							placeholder: 'It steered us towards success',
+							initial_value: payload.text ?? '',
+							hint: 'Your comments will help us improve our process'
+						}),
+					]
+				})
+			)
 
-	// TODO handle interactivity (e.g. context commands, modals)
+			if (!response.ok) {
+				console.log('Error', response)
+			}
+
+			break;
+		
+		default:
+			return {
+				statusCode: 200,
+				body: `command ${payload.command} is not recognized`
+			}
+	}
 
 	return {
 		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
+		body: ''
+	}
+}
+
+const handleInteractivity = async (payload:SlackModalPayload) => {
+	const callback_id = payload.callback_id ?? payload.view.callback_id;
+
+	switch(callback_id) {
+
+		case 'foodfight-modal':
+			const data = payload.view.state.values;
+			const fields = {
+				opinions: data.opinion_block.opinion.value,
+				spiceLevel: data.spice_level_block.spice_level.selected_option.value,
+				submitter: payload.user.id,
+				submitterName: payload.user.name
+			}
+			console.log(fields);
+			await slackApi('chat.postMessage', {
+				channel: 'C05SC31JZ0Q',
+				text: `oh dang, ya'll  <@${fields.submitter}>`
+			})
+
+			break;
+
+			default:
+				console.log('No handler defined for', callback_id)
+				return {
+					statusCode: 400,
+					body: `No Handler found for ${callback_id}`
+				}
+	}
+
+	return {
+		statusCode: 200,
+		body: ''
+	} 
+}
+
+const handleTextCommand = async (body:SlackSlashCommandPayload) => {
+	const { text } = body as SlackSlashCommandPayload
+	const usernames = getUsernames(text);
+	const channel = usernames[0]
+
+	await slackApi('chat.postMessage', {
+		channel: channel,
+		text: `oh dang, ya'll`
+	})
+	return {
+		statusCode: 200,
+		body: ''
+	} 
+}
+
+export const handler: Handler = async (event) => {
+	const valid = verifySlackRequest(event);
+	if (!valid) {
+		console.log('invalid request')
+
+		return {
+			statusCode: 400,
+			body: 'invalid request'
+		}
+	}
+
+	// TODO handle slash commands
+	const body = parse(event.body ?? '') as SlackPayload;
+	const { text } = body as SlackSlashCommandPayload
+
+	if (text) {
+		return handleTextCommand(body as SlackSlashCommandPayload);
+	}
+
+	if (body.command) {
+		return handleSlashCommand(body as SlackSlashCommandPayload);
+	}
+
+	
+
+	const payload = JSON.parse(body.payload)
+	// TODO handle interactivity (e.g. context commands, modals)
+	if (payload) {
+		return handleInteractivity(payload)
+	}
+
+	return {
+		statusCode: 200,
+		body: 'TODO: handle Slack commands and interactivity!!',
 	};
 };
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,1 +1,178 @@
-// TODO create Slack utilities
+import { HandlerEvent } from "@netlify/functions";
+import { createHmac } from "crypto";
+
+import fetch from "node-fetch";
+import { title } from "process";
+
+export const slackApi = (endpoint: SlackApiEndpoint, body: SlackApiRequestBody) => {
+    return fetch(`https://slack.com/api/${endpoint}`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+            'Content-Type': 'application/json; charset=utf-8',
+            as_user: 'true'
+        },
+        body: JSON.stringify(body)
+    }).then(res=> res.json())
+}
+
+export const verifySlackRequest = (request: HandlerEvent) => {
+    const secret = process.env.SLACK_SIGNIN_SECRET!;
+    const signature = request.headers['x-slack-signature']
+    const timestamp = Number(request.headers['x-slack-request-timestamp'])
+
+    const now = Math.floor(Date.now()/1000);
+
+    if (Math.abs(now - timestamp) > 300) {
+        return false;
+    }
+
+    const hash = createHmac('sha256', secret)
+    .update(`v0:${timestamp}:${request.body}`)
+    .digest('hex');
+
+    return `v0=${hash}` === signature;
+}
+
+export const blocks = {
+    section: ({text}: SectionBlockArgs): SlackBlockSection => {
+        return {
+            type: 'section',
+            text: {
+                type: 'mrkdwn',
+                text: text
+            }
+        }
+    },
+    input: ({
+        id, label, placeholder, initial_value = '', hint=''
+    }: InputBlockArgs): SlackBlockInput => {
+        return {
+            block_id: `${id}_block`,
+            type: 'input',
+            label : {
+                type: 'plain_text',
+                text: label
+            },
+            element: {
+                action_id: id,
+                type: 'plain_text_input',
+                placeholder: {
+                    type: 'plain_text',
+                    text: placeholder
+                },
+                initial_value
+            },
+            hint: {
+                type: 'plain_text',
+                text: hint
+            }
+        }
+    },
+    select: ({
+        id, label, 
+        placeholder, options
+    }: SelectBlockArgs): SlackBlockInput => {
+        return {
+            block_id: `${id}_block`,
+            type: 'input',
+            label : {
+                type: 'plain_text',
+                text: label
+            },
+            element : {
+                action_id: id,
+                type: 'static_select',
+                placeholder: {
+                    type: 'plain_text',
+                    text: placeholder,
+                    emoji: true
+                },
+                options: options.map(({label, value}) => {
+                    return { 
+                        text: {
+                            type: 'plain_text',
+                            text: label,
+                            emoji: true
+                        },
+                        value
+                    }
+                })
+            }
+        }
+    },
+    radios: ({
+        id, label, 
+        placeholder, options
+    }: SelectBlockArgs): SlackBlockInput => {
+        return {
+            block_id: `${id}_block`,
+            type: 'input',
+            label : {
+                type: 'plain_text',
+                text: label
+            },
+            element : {
+                action_id: id,
+                type: 'radio_buttons',
+                options: options.map(({label, value}) => {
+                    return { 
+                        text: {
+                            type: 'plain_text',
+                            text: label,
+                            emoji: true
+                        },
+                        value
+                    }
+                })
+            }
+        }
+    }
+}
+
+export function  modal ({
+    trigger_id,
+    id,
+    submit_text = 'Submit',
+    blocks
+}: ModalArgs) {
+    return {
+        trigger_id,
+        view: {
+            type: 'modal',
+            callback_id: id,
+            title: {
+                type: 'plain_text',
+                text: title
+            },
+            submit: {
+                type: 'plain_text',
+                text: submit_text
+            },
+            blocks
+        }
+    }
+}
+
+const  onlyUnique = (value: string, index: number, items: string[]) => {
+    return items.indexOf(value) === index;
+}
+
+export const getUsernames = (text: string = '') => {
+    if (!text) {
+      return []
+    }
+  
+    const tokens = text.split(' ')
+    const usernames = tokens.reduce((acc: string[], token: string) : string[] => {
+      const arrowsReplaced = token.replace('<', '').replace('>', '')
+      const username = arrowsReplaced.split('|')
+      if (username.length == 2) {
+        acc.push(username[0])
+      }
+      return acc
+    }, [])
+
+    return usernames.filter(onlyUnique);
+  }
+  
